ec5c6144206571db1ea2a744e46a9776b951908a
chandm8 <chandm8@vt.edu>
2020-03-23 01:40:34 -0400

    Finished music app

--- mysite/static/js/home.js
+++ mysite/static/js/home.js
@@ -76,33 +76,3 @@ $(document).ready(function() {
     setTimeout(typeWriter, 3000);
 
 });
-
-var first = true;
-var capInterval;
-
-function funnyTxt(x) {
-    var randIdx = 0;
-    capInterval = setInterval(function() {
-        x.innerHTML = x.innerHTML.toLowerCase();
-        if (first) {
-            randIdx = Math.floor(Math.random() * x.innerHTML.length);
-            x.innerHTML = x.innerHTML.substring(0, randIdx) + x.innerHTML.charAt(randIdx).toUpperCase() + x.innerHTML.substring(randIdx + 1, x.innerHTML.length);
-            first = false
-        }
-        else {
-            var randTmp = Math.floor(Math.random() * x.innerHTML.length);
-            while (randTmp == randIdx) {
-                randTmp = Math.floor(Math.random() * x.innerHTML.length);
-            }
-            x.innerHTML = x.innerHTML.substring(0, randTmp) + x.innerHTML.charAt(randTmp).toUpperCase() + x.innerHTML.substring(randTmp + 1, x.innerHTML.length);
-            randIdx = randTmp;
-        }
-
-    }, 150);
-}
-
-function fixTxt(x) {
-    clearInterval(capInterval);
-    var first = true; //reset in onmouseout
-    x.innerHTML = x.innerHTML.toLowerCase();
-}
--- mysite/static/js/overall.js
+++ mysite/static/js/overall.js
@@ -4,11 +4,52 @@ function darkTheme() {
     if (darkOn) {
         document.body.style.backgroundColor = "white";
         document.body.style.color = "black";
+        var anchors = document.getElementsByClassName("anchor");
+        for(var i=0, len=anchors.length; i<len; i++)
+        {
+            anchors[i].style["color"] = "black";
+        }
+        darkOn = true;
         darkOn = false;
     }
     else {
         document.body.style.backgroundColor = "black";
         document.body.style.color = "white";
+        var anchors = document.getElementsByClassName("anchor");
+        for(var i=0, len=anchors.length; i<len; i++)
+        {
+            anchors[i].style["color"] = "white";
+        }
         darkOn = true;
     }
+}
+
+var first = true;
+var capInterval;
+
+function funnyTxt(x) {
+    var randIdx = 0;
+    capInterval = setInterval(function() {
+        x.innerHTML = x.innerHTML.toLowerCase();
+        if (first) {
+            randIdx = Math.floor(Math.random() * x.innerHTML.length);
+            x.innerHTML = x.innerHTML.substring(0, randIdx) + x.innerHTML.charAt(randIdx).toUpperCase() + x.innerHTML.substring(randIdx + 1, x.innerHTML.length);
+            first = false
+        }
+        else {
+            var randTmp = Math.floor(Math.random() * x.innerHTML.length);
+            while (randTmp == randIdx) {
+                randTmp = Math.floor(Math.random() * x.innerHTML.length);
+            }
+            x.innerHTML = x.innerHTML.substring(0, randTmp) + x.innerHTML.charAt(randTmp).toUpperCase() + x.innerHTML.substring(randTmp + 1, x.innerHTML.length);
+            randIdx = randTmp;
+        }
+
+    }, 150);
+}
+
+function fixTxt(x) {
+    clearInterval(capInterval);
+    var first = true; //reset in onmouseout
+    x.innerHTML = x.innerHTML.toLowerCase();
 }
